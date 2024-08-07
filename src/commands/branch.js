6da22ebcfe3d0fc75d9f9a8c8ef388b013670a3d
David Umoru <umorudavido@gmail.com>
2024-08-07 20:46:30 +0100

    Implement branch commands

---
+++ src/commands/branch.js
@@ -0,0 +1,56 @@
+const simpleGit = require("simple-git");
+const inquirer = require("inquirer");
+const git = simpleGit();
+
+const createBranch = async () => {
+  const { branchName } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "branchName",
+      message: "Enter the branch name:",
+    },
+  ]);
+
+  try {
+    await git.branch([branchName]);
+    console.log(`Branch ${branchName} created`);
+  } catch (err) {
+    console.error("Error creating branch:", err);
+  }
+};
+
+const switchBranch = async () => {
+  const { branchName } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "branchName",
+      message: "Enter the branch name to switch to:",
+    },
+  ]);
+
+  try {
+    await git.checkout(branchName);
+    console.log(`Switched to branch ${branchName}`);
+  } catch (err) {
+    console.error("Error switching branch:", err);
+  }
+};
+
+const deleteBranch = async () => {
+  const { branchName } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "branchName",
+      message: "Enter the branch name to delete:",
+    },
+  ]);
+
+  try {
+    await git.branch(["-d", branchName]);
+    console.log(`Branch ${branchName} deleted`);
+  } catch (err) {
+    console.error("Error deleting branch:", err);
+  }
+};
+
+module.exports = { createBranch, switchBranch, deleteBranch };
